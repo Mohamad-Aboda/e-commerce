74b165479083f081e2a9aac042cca7d82e2e2882
Mohamad-Aboda <MohamedAboda@std.mans.edu.eg>
2023-09-04 23:30:47 +0300

    Add some helper function to simplify logic

--- products/utils.py
+++ products/utils.py
@@ -1,2 +1,10 @@
 def product_image_directory_path(inst, file_name):
     return f"product_images/{inst.product.name}/{file_name}"
+
+
+def multiple_image_upload(request):
+    if isinstance(request.FILES.getlist("image"), list) and len(request.FILES.getlist("image")) > 1:
+        return True
+    return False
+
+
