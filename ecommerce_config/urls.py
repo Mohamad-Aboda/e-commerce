21ca61a27f4865dd568ee6200e731dbfeaeddc8a
Mohamad-Aboda <MohamedAboda@std.mans.edu.eg>
2023-09-04 23:30:02 +0300

    Add routes for the product app

--- ecommerce_config/urls.py
+++ ecommerce_config/urls.py
@@ -3,11 +3,13 @@ from django.urls import path, include, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf.urls.static import static
+from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
         title="e-commerce",
-        default_version='v1',
+        default_version="v1",
         description="e-commerce",
         terms_of_service="https://www.yourapp.com/terms/",
         contact=openapi.Contact(email="contact@yourapp.com"),
@@ -18,20 +20,22 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls',   namespace='accounts')),
-    path('api/', include('categories.urls', namespace='categories')),
-
-
-
-
-
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-
+    path("admin/", admin.site.urls),
+    path("api/", include("accounts.urls", namespace="accounts")),
+    path("api/", include("categories.urls", namespace="categories")),
+    path("api/", include("products.urls", namespace="products")),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
 
 
-
-
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
