95ddeabc19965c341d83dff1f6f8a31753516cd2
Mohamad-Aboda <MohamedAboda@std.mans.edu.eg>
2023-09-08 15:10:50 +0300

    update the category url to include category as prefix

--- categories/urls.py
+++ categories/urls.py
@@ -4,8 +4,8 @@ from .apis import CategoryListCreateView, CategoryRetriveUpdateDestroyView
 app_name = 'categories'
 
 urlpatterns = [
-    path('list-create/', CategoryListCreateView.as_view(), name='category-list-create'),
-    path('retrive-update-delete/<int:pk>/', CategoryRetriveUpdateDestroyView.as_view(), name='category-retrive-update-delete'),
+    path('category/list-create/', CategoryListCreateView.as_view(), name='category-list-create'),
+    path('category/retrive-update-delete/<int:pk>/', CategoryRetriveUpdateDestroyView.as_view(), name='category-retrive-update-delete'),
 ]
 
 
