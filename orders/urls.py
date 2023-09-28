da1a6a3adfd7516d590631538d2e72a275feeb69
Mohamad-Aboda <MohamedAboda@std.mans.edu.eg>
2023-09-28 12:36:22 +0300

    Update the routes to include the urls for order items

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .apis import (OrderListCreateView, OrderRetrieveDestroyView)
+from .apis import (OrderListCreateView, OrderRetrieveDestroyView,
+                    OrderItemListCreateView, OrderItemRetrieveUpdateDestroyView)
 
 app_name = 'orders'
 
@@ -7,6 +8,6 @@ urlpatterns = [
     path('orders/', OrderListCreateView.as_view(), name='order-list-create'),
     path('orders/<int:order_pk>/', OrderRetrieveDestroyView.as_view(), name='order-retrieve-destroy'),
 
-    # path('orders/<int:order_pk>/items/', views.OrderItemListCreateView.as_view(), name='order-item-list-create'),
-    # path('orders/<int:order_pk>/items/<int:item_pk>/', views.OrderItemRetrieveUpdateDestroyView.as_view(), name='order-item-retrieve-update-destroy'),
+    path('orders/<int:order_pk>/items/', OrderItemListCreateView.as_view(), name='order-item-list-create'),
+    path('orders/<int:order_pk>/items/<int:item_pk>/', OrderItemRetrieveUpdateDestroyView.as_view(), name='order-item-retrieve-update-destroy'),
 ]
