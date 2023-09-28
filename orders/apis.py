2e54c8cadbe314d59bc88b839fe8d5e7e3ba3225
Mohamad-Aboda <MohamedAboda@std.mans.edu.eg>
2023-09-28 12:36:46 +0300

    Added endpoint logic crud for order items

--- orders/apis.py
+++ orders/apis.py
@@ -8,8 +8,8 @@ from drf_yasg.utils import swagger_auto_schema
 
 
 from .models import Order, OrderItem
-from .serializers import OrderSerializer, OrderItemSerializer
-from .permissions import ProductListCreateCustomPermission, ProductRetrieveDestroyPermission
+from .serializers import OrderSerializer, OrderItemSerializer, OrderItemCreateUpdateSerializer
+from .permissions import ProductListCreateCustomPermission, ProductRetrieveDestroyPermission, ProductItemPermission
 
 User = get_user_model()
 
@@ -41,8 +41,6 @@ class OrderListCreateView(APIView):
         else:
             return Response({"detail": "Authentication credentials were not provided."},
                             status=status.HTTP_401_UNAUTHORIZED)
-
-
 
     @swagger_auto_schema(
         request_body=OrderSerializer,
@@ -118,83 +116,142 @@ class OrderRetrieveDestroyView(APIView):
                             status=status.HTTP_401_UNAUTHORIZED)
 
 
+class OrderItemListCreateView(APIView):
+    permission_classes = [ProductItemPermission]
+
+    def get_order(self, order_pk):
+        try:
+            order = Order.objects.get(pk=order_pk)
+            # Check if the order belongs to the authenticated user
+            if order.user == self.request.user:
+                return order
+            else:
+                return None
+        except Order.DoesNotExist:
+            return None
+
+    @swagger_auto_schema(
+        responses={status.HTTP_200_OK: OrderItemSerializer(many=True)},
+        manual_parameters=[
+            openapi.Parameter(
+                name="Authorization",
+                in_=openapi.IN_HEADER,
+                type=openapi.TYPE_STRING,
+                description="Bearer token: 'Bearer {token}'",
+            ),
+        ],
+    )
+    def get(self, request, order_pk):
+        order = self.get_order(order_pk)
+        if order:
+            items = OrderItem.objects.filter(order=order)
+            if items.count() > 0:
+                serializer = OrderItemSerializer(items, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response({"detail": "Order does not contains items yet."}, status=status.HTTP_200_OK)
+
+        return Response({"detail": "Order not found or you don't have permission."}, status=status.HTTP_404_NOT_FOUND)
+
+    @swagger_auto_schema(
+        request_body=OrderItemCreateUpdateSerializer,
+        responses={status.HTTP_201_CREATED: OrderItemCreateUpdateSerializer()},
+        manual_parameters=[
+            openapi.Parameter(
+                name="Authorization",
+                in_=openapi.IN_HEADER,
+                type=openapi.TYPE_STRING,
+                description="Bearer token: 'Bearer {token}'",
+            ),
+        ],
+    )
+    def post(self, request, order_pk):
+        """ Add items to existing order """
+        order = self.get_order(order_pk)
+        if order:
+            serializer = OrderItemCreateUpdateSerializer(data=request.data)
+            if serializer.is_valid():
+                # Check if the product belongs to the authenticated user
+                product = serializer.validated_data['product']
+                if product.user == request.user:
+                    serializer.save(order=order)
+                    return Response({"msg":"items added to order succefully", "details":serializer.data}, status=status.HTTP_201_CREATED)
+                else:
+                    return Response({"detail": "You don't have permission to add this item."}, status=status.HTTP_403_FORBIDDEN)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"detail": "Order not found or you don't have permission."}, status=status.HTTP_404_NOT_FOUND)
 
 
 
+class OrderItemRetrieveUpdateDestroyView(APIView):
+    permission_classes = [ProductItemPermission]
+
+    def get_order_item(self, order_pk, item_pk):
+        try:
+            order_item = OrderItem.objects.get(order__pk=order_pk, pk=item_pk)
+            # Check if the order item belongs to the authenticated user
+            if order_item.order.user == self.request.user:
+                return order_item
+            else:
+                return None
+        except OrderItem.DoesNotExist:
+            return None
 
 
+    @swagger_auto_schema(
+        responses={status.HTTP_200_OK: OrderItemSerializer()},
+        manual_parameters=[
+            openapi.Parameter(
+                name="Authorization",
+                in_=openapi.IN_HEADER,
+                type=openapi.TYPE_STRING,
+                description="Bearer token: 'Bearer {token}'",
+            ),
+        ],
+    )
+    def get(self, request, order_pk, item_pk):
+        order_item = self.get_order_item(order_pk, item_pk)
+        if order_item:
+            serializer = OrderItemSerializer(order_item)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"detail": "Order item not found or you don't have permission."}, status=status.HTTP_404_NOT_FOUND)
 
-# class OrderItemListCreateView(APIView):
-#     permission_classes = [IsAuthenticated]
+    @swagger_auto_schema(
+        request_body=OrderItemCreateUpdateSerializer,
+        responses={status.HTTP_200_OK: OrderItemCreateUpdateSerializer()},
+        manual_parameters=[
+            openapi.Parameter(
+                name="Authorization",
+                in_=openapi.IN_HEADER,
+                type=openapi.TYPE_STRING,
+                description="Bearer token: 'Bearer {token}'",
+            ),
+        ],
+    )
+    def put(self, request, order_pk, item_pk):
+        order_item = self.get_order_item(order_pk, item_pk)
+        if order_item:
+            serializer = OrderItemCreateUpdateSerializer(order_item, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"detail": "Order item not found or you don't have permission."}, status=status.HTTP_404_NOT_FOUND)
 
-#     def get_order(self, order_pk):
-#         try:
-#             order = Order.objects.get(pk=order_pk)
-#             # Check if the order belongs to the authenticated user
-#             if order.user == self.request.user:
-#                 return order
-#             else:
-#                 return None
-#         except Order.DoesNotExist:
-#             return None
-
-#     def get(self, request, order_pk):
-#         order = self.get_order(order_pk)
-#         if order:
-#             items = OrderItem.objects.filter(order=order)
-#             serializer = OrderItemSerializer(items, many=True)
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         return Response({"detail": "Order not found or you don't have permission."}, status=status.HTTP_404_NOT_FOUND)
-
-#     def post(self, request, order_pk):
-#         order = self.get_order(order_pk)
-#         if order:
-#             serializer = OrderItemSerializer(data=request.data)
-#             if serializer.is_valid():
-#                 # Check if the product belongs to the authenticated user
-#                 product = serializer.validated_data['product']
-#                 if product.user == request.user:
-#                     serializer.save(order=order)
-#                     return Response(serializer.data, status=status.HTTP_201_CREATED)
-#                 else:
-#                     return Response({"detail": "You don't have permission to add this item."}, status=status.HTTP_403_FORBIDDEN)
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#         return Response({"detail": "Order not found or you don't have permission."}, status=status.HTTP_404_NOT_FOUND)
-
-# class OrderItemRetrieveUpdateDestroyView(APIView):
-#     permission_classes = [IsAuthenticated]
-
-#     def get_order_item(self, order_pk, item_pk):
-#         try:
-#             order_item = OrderItem.objects.get(order__pk=order_pk, pk=item_pk)
-#             # Check if the order item belongs to the authenticated user
-#             if order_item.order.user == self.request.user:
-#                 return order_item
-#             else:
-#                 return None
-#         except OrderItem.DoesNotExist:
-#             return None
-
-#     def get(self, request, order_pk, item_pk):
-#         order_item = self.get_order_item(order_pk, item_pk)
-#         if order_item:
-#             serializer = OrderItemSerializer(order_item)
-#             return Response(serializer.data, status=status.HTTP_200_OK)
-#         return Response({"detail": "Order item not found or you don't have permission."}, status=status.HTTP_404_NOT_FOUND)
-
-#     def put(self, request, order_pk, item_pk):
-#         order_item = self.get_order_item(order_pk, item_pk)
-#         if order_item:
-#             serializer = OrderItemSerializer(order_item, data=request.data)
-#             if serializer.is_valid():
-#                 serializer.save()
-#                 return Response(serializer.data, status=status.HTTP_200_OK)
-#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#         return Response({"detail": "Order item not found or you don't have permission."}, status=status.HTTP_404_NOT_FOUND)
-
-#     def delete(self, request, order_pk, item_pk):
-#         order_item = self.get_order_item(order_pk, item_pk)
-#         if order_item:
-#             order_item.delete()
-#             return Response({"detail": "Order item deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
-#         return Response({"detail": "Order item not found or you don't have permission."}, status=status.HTTP_404_NOT_FOUND)
+    @swagger_auto_schema(
+        responses={status.HTTP_204_NO_CONTENT: "Order item deleted successfully."},
+        manual_parameters=[
+            openapi.Parameter(
+                name="Authorization",
+                in_=openapi.IN_HEADER,
+                type=openapi.TYPE_STRING,
+                description="Bearer token: 'Bearer {token}'",
+            ),
+        ],
+    )
+    def delete(self, request, order_pk, item_pk):
+        order_item = self.get_order_item(order_pk, item_pk)
+        if order_item:
+            order_item.delete()
+            return Response({"detail": "Order item deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+        return Response({"detail": "Order item not found or you don't have permission."}, status=status.HTTP_404_NOT_FOUND)
